3154c77405892f88acf933c4e5d784b00b8ce2d1
Correx1 <rexp795@gmail.com>
2026-03-19 07:57:23 +0100

    updated

--- middleware.ts
+++ middleware.ts
@@ -10,8 +10,8 @@ export async function middleware(request: NextRequest) {
 
   // Create isolated edge cookie storage strictly refreshing dynamic states!
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://dummy.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy',
     {
       cookies: {
         getAll() {
